17263e6dade99fe54f213aa3b6738e9aad620416
Praveen851 <rpraveenbabu851@gmail.com>
2024-09-02 16:41:21 +0530

    add picture list constant

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const PictureList = [
+    {
+        id: 1,
+        url: "https://plus.unsplash.com/premium_photo-1676496046182-356a6a0ed002?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        onClick: () => console.log("hi 1"),
+    },
+    {
+        id: 2,
+        url: "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        onClick: () => console.log("hi 2"),
+    },
+    {
+        id: 3,
+        url: "https://plus.unsplash.com/premium_photo-1720886696626-c7a35ab957d7?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        onClick: () => console.log("hi 3"),
+    },
+];
